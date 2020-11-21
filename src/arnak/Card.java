3e141cd4d53c0cda69e983d4d206dda7fbd7bfa3
myfriendducky <72713545+myfriendducky@users.noreply.github.com>
2020-11-21 10:23:05 -0800

    Update Card.java

--- src/arnak/Card.java
+++ src/arnak/Card.java
@@ -1,42 +1,59 @@
-package arnak;
+// stores and manipulates data related to cards
+
+package arnak_test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Card
 {
-	Player player;
+	Player owner;
+	Effect effect = new Effect();
 	
-	String name;
-	String type;
-	String travelValue;
+	Map<String, String> cardInfo = new HashMap<String, String>();
 	
-	boolean freeAction;
+	boolean free;
 	
-	public Card(Player owner)
+	public Card()
 	{
-		player = owner;
+		free = false;
 	}
 	
-	public Player getOwner()
+	// sets the owner this card belongs to, should be used when giving out this card to a player
+	public void setOwner(Player player)
 	{
-		return player;
+		owner = player;
 	}
 	
-	public String getName()
+	// pass in a String and get the appropriate info stored on the card
+	public String getInfo(String target)
 	{
-		return name;
+		return cardInfo.get(target);
 	}
 	
-	public String getType()
+	// sets the info to the target, for example, set "name" to "funding", by default there's no info on
+	// the card, so this should be used when making a card. Also, make sure one of the info stored is
+	// called "effectID" and that should be storing a String of the effect this card is tied to
+	public void setInfo(String target, String result)
 	{
-		return type;
+		cardInfo.put(target, result);
 	}
 	
-	public String getTravelValue()
+	// sets the card's effect to free action, by default it's not free
+	public void setToFree()
 	{
-		return travelValue;
+		free = true;
 	}
 	
+	// gives back whether this card's effect is a free action
 	public boolean isFree()
 	{
-		return freeAction;
+		return free;
+	}
+	
+	// uses the effect this card is attached to
+	public void resolveEffect()
+	{
+		effect.resolveEffect(getInfo("effectID"), owner);
 	}
 }
